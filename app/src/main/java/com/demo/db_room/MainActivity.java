d2d3742f531cf9256fdf90d5fadc8d8dcf65d3eb
Mateo Contreras <macontrerasc@ufpso.edu.co>
2022-08-04 16:06:53 -0500

    IMPLEMENTACIÓN
    - Se agrega la nueva clase 'Gestionar.java' ejecutar las acciones de actualizar y eliminar los registros.

--- app/src/main/java/com/demo/db_room/MainActivity.java
+++ app/src/main/java/com/demo/db_room/MainActivity.java
@@ -2,13 +2,37 @@ package com.demo.db_room;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button btn_agg, btn_gestionar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btn_agg = findViewById(R.id.btn_agg);
+        btn_gestionar = findViewById(R.id.btn_gestionar);
+
+        btn_agg.setOnClickListener(view -> {
+            runIntent(Registrar.class);
+        });
+
+        btn_gestionar.setOnClickListener(view -> {
+            runIntent(Gestionar.class);
+        });
+
     }
+
+    private void runIntent(Class c) {
+        Intent i = new Intent(this, c);
+        startActivity(i);
+    }
+
+
 }
