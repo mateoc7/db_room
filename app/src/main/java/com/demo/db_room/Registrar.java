51521e1d3f1dd7b427f06a22d62c8b6b6d3e7fab
Mateo Contreras <macontrerasc@ufpso.edu.co>
2022-08-04 15:42:27 -0500

    IMPLEMENTACIÓN
    - Se agrega diseño base.

---
+++ app/src/main/java/com/demo/db_room/Registrar.java
@@ -0,0 +1,14 @@
+package com.demo.db_room;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Registrar extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_registrar);
+    }
+}
