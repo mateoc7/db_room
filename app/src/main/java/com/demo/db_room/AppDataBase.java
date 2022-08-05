22be8efdb530e9c3a0671249b2b39ac5ea54c660
Mateo Contreras <macontrerasc@ufpso.edu.co>
2022-08-05 08:40:16 -0500

    IMPLEMENTACIÓN
    - Se crea la clase 'AppDataBase' para contener la base de datos. Esta efine la configuración de la base de datos y sirve como el punto de acceso principal de la app a los datos.

---
+++ app/src/main/java/com/demo/db_room/AppDataBase.java
@@ -0,0 +1,12 @@
+package com.demo.db_room;
+
+import androidx.room.Database;
+import androidx.room.RoomDatabase;
+
+import com.demo.db_room.Interface.UserDao;
+import com.demo.db_room.Modelo.Usuario;
+
+@Database(entities = {Usuario.class}, version = 1)
+public abstract class AppDataBase extends RoomDatabase {
+    public abstract UserDao userDao();
+}
