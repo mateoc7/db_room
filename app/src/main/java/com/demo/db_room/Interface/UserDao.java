80f70f69313d05b04080b8f3e62e93b1bdcceb94
Mateo Contreras <macontrerasc@ufpso.edu.co>
2022-08-05 08:36:49 -0500

    IMPLEMENTACIÓN
    - Se crea la interfaz que contiene los métodos correspondientes al CRUD.
    - '@Insert' para insertar nuevas filas de a la tabla, recibe por parametro uno o más objetos de tipo 'Usuario'.
    - '@Query' permite realizar instrucciones de SQL y exponerlas como métodos DAO.
    - '@Update' permite actualizar filas especificas en una tabla.
    - '@Delete' permite eliminar filas especificas en una tabla.

---
+++ app/src/main/java/com/demo/db_room/Interface/UserDao.java
@@ -0,0 +1,36 @@
+package com.demo.db_room.Interface;
+
+import androidx.room.Dao;
+import androidx.room.Delete;
+import androidx.room.Insert;
+import androidx.room.Query;
+import androidx.room.Update;
+
+import com.demo.db_room.Modelo.Usuario;
+
+import java.util.List;
+
+@Dao
+public interface UserDao {
+
+    //Create
+    @Insert
+    void insertAll(Usuario... usuarios);
+
+    //Read
+    @Query("SELECT * FROM users")
+    List<Usuario> getAll();
+
+    //Update
+    @Update
+    void updateUsers(Usuario... usuarios);
+
+    //Delete
+    @Delete
+    void delete(Usuario usuario);
+
+    //Pasar parametros simples a una consulta (búsqueda)
+    @Query("SELECT * FROM users WHERE name = :ref")
+    Usuario[] loadUsersSameName(String ref);
+
+}
